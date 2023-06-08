ccdfd53ead212361c626d46568a698d57390b7f0
Alexander Madyankin <alexander@madyankin.name>
2023-06-08 12:55:47 +0200

    Fix Encoding::UndefinedConversionError when trying to save the tempfile

--- lib/rails_dominant_colors/url.rb
+++ lib/rails_dominant_colors/url.rb
@@ -46,7 +46,7 @@ module RailsDominantColors
     end
 
     def tempfile(extension, body)
-      tempfile = Tempfile.open(['source', extension])
+      tempfile = Tempfile.open(['source', extension], binmode: true)
       tempfile.write(body)
       tempfile.close
       tempfile.path
